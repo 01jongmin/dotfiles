4ad38308d0816b47acd02128c58eb51fc3904ae2
Jong Min Choi <01jongminchoi@gmail.com>
2022-03-13 17:03:22 -0400

    Use org mode to create init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,26 +1,3 @@
-;; UI
-(setq inhibit-startup-message t)
-
-(scroll-bar-mode -1)              ; Disable visible scrollbar
-(tool-bar-mode -1)                ; Disable the toolbar
-(tooltip-mode -1)                 ; Disable tooltips
-(set-fringe-mode 10)              ; Give some breathing room
-
-(menu-bar-mode -1)                ; Disable the menu bar
-
-
-;; Font
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 170)
-
-;; Modifier Key remap
-(setq mac-command-modifier 'control)
-
-(setq ring-bell-function 'ignore)
-(setq scroll-step 1
-      scroll-conservatively 10000)
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
 ;; Initialize package sources
 (require 'package)
 
@@ -39,22 +16,79 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)              ; Disable visible scrollbar (tool-bar-mode -1)
+(tool-bar-mode -1)                ; Disable the toolbar
+(tooltip-mode -1)                 ; Disable tooltips
+(set-fringe-mode 10)              ; Give some breathing room
+
+(menu-bar-mode -1)                ; Disable the menu bar
+
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
+(setq ring-bell-function 'ignore)
+(setq scroll-step 1
+        scroll-conservatively 10000)
 ;; Disable line numbers for some modes
 (dolist (mode '(org-mode-hook
-		term-mode-hook
-		shell-mode-hook
-		eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+    (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(set-face-attribute 'default nil :font "fira code retina" :height 170)
+
+;; Modifier Key remap
+(setq mac-command-modifier 'control)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+
+(use-package general
+  :config
+  (general-create-definer jongmin/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "M-SPC")
+
+  (jongmin/leader-keys
+    "t" '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose themes")))
+
+(general-define-key "C-M-j" 'counsel-switch-buffer)
+
+
+(jongmin/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1)
+
+  ;; Use visual line mode
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'message-buffer-mode 'normal)
+)
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history))
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
   :config
   (setq ivy-initial-inputs-alist nil)) ;; Don't start search with ^
 
@@ -82,6 +116,26 @@
   :init
   (ivy-rich-mode 1))
 
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
 ;; First time when you load these configuration you need to run the following commands
 ;; M-x all-the-icons-install-fonts
 (use-package all-the-icons
@@ -95,69 +149,17 @@
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;; Shows options for commands
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.3))
 
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
+(use-package magit)
 
-(use-package general
+(use-package forge
   :config
-  (general-create-definer jongmin/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "M-SPC")
- 
-  (jongmin/leader-keys
-    "t" '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose themes")))
-
-(general-define-key
- "C-M-j" 'counsel-switch-buffer)
-
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1)
-
-  ;; Use visual line mode
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'message-buffer-mode 'normal)
-)
-
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
-
-(use-package hydra)
-
-(defhydra hydra-text-scale (:timeout 4)
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
-
-(jongmin/leader-keys
-  "ts" '(hydra-text-scale/body :which-key "scale text"))
-
+  (setq auth-source '("~/.authinfo")))
 
 (use-package projectile
   :diminish projectile-mode
@@ -174,21 +176,72 @@
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
 
-(use-package magit)
+(defun jongmin/org-mode-setup ()
+  (org-indent-mode)
+  (visual-line-mode 1))
 
-(setq auth-sources '("~/.authinfo"))
-(use-package forge)
+(use-package org
+  :hook (org-mode . jongmin/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(forge magit counsel-projectile which-key use-package rainbow-delimiters projectile ivy-rich hydra helpful general ewal-doom-themes evil-collection doom-modeline counsel command-log-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  (setq org-agenda-start-with-log-mode t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+  (setq org-todo-keywords
+        '((sequence "TODO" "PROGRESS" "|" "DONE" "BLOCKED")))
+  (setq org-agenda-files
+        '("~/OrgFiles/Tasks.org"
+          "~/OrgFiles/Birthdays.org")))
+  ;(jongmin/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun jongmin/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :defer t
+  :hook (org-mode . jongmin/org-mode-visual-fill))
+
+;; Automatically tangle our Emacs.org config file when we save it
+(defun jongmin/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "/Users/jongmin/dotfiles/emacs/Emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'jongmin/org-babel-tangle-config)))
+
+(use-package mu4e
+  :load-path  "/usr/local/share/emacs/site-lisp/mu/mu4e/"
+  :defer 20
+  :config
+  (setq mu4e-mu-binary (executable-find "mu"))
+
+  ;; this is the directory we created before:
+  (setq mu4e-maildir "~/.maildir")
+
+  ;; this command is called to sync imap servers:
+  (setq mu4e-get-mail-command (concat (executable-find "mbsync") " -a"))
+  ;; how often to call it in seconds:
+  (setq mu4e-update-interval 300)
+
+  ;; save attachment to desktop by default
+  ;; or another choice of yours:
+  (setq mu4e-attachment-dir "~/Desktop")
+
+  ;; rename files when moving - needed for mbsync:
+  (setq mu4e-change-filenames-when-moving t)
+
+  ;; list of your email adresses:
+  (setq mu4e-user-mail-address-list '("jongmin@seas.upenn.edu"))
+
+  (mu4e t))
